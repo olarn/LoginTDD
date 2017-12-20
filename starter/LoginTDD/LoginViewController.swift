96734e42fb08b6be5ba98ab9c9d2b04f311945c6
Olarn U <olarn.u@gmail.com>
2017-12-20 13:39:17 +0700

    Complete prepare starter project. Ready for TDD.

---
+++ starter/LoginTDD/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  LoginTDD
+//
+//  Created by Olarn U. on 20/12/2560 BE.
+//  Copyright © 2560 Olarn U. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
---
+++ starter/LoginTDDTests/LoginViewModelTests.swift
@@ -0,0 +1,24 @@
+//
+//  LoginTDDTests.swift
+//  LoginTDDTests
+//
+//  Created by Olarn U. on 20/12/2560 BE.
+//  Copyright © 2560 Olarn U. All rights reserved.
+//
+
+import XCTest
+@testable import LoginTDD
+
+class LoginViewModelTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+}
